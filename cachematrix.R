6b8d005c09456ce9f33a5d5fe7aee690b3a6505c
Tsanislav <tsanislav@gmail.com>
2014-11-23 15:52:46 +0100

    # makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse. # cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. # If the inverse has already been calculated (and the matrix has not changed), then the cachesolve retrieves # the inverse from the cache.
    Signed-off-by: Tsanislav <tsanislav@gmail.com>

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,63 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# The makeCacheMatrix function creates a list, which contains the function to
+#     - set the value of a square matrix
+#     - get the value of a square matrix
+#     - set the value of the inverse matrix (solve)
+#     - get the value of the inverse matrix (solve)
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
 
+  # Below we create the list with the set/get values
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse) 
 }
 
-
-## Write a short comment describing this function
+# The cacheSolve function returns the inverse of the matrix,
+# checking if it has already been calculated. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  
+  # If the m variable already contains the inverse values
+  # of the matrix, it will return the message "getting cached
+  # data", along with the inversed matrix.
+  
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
+
+# This is work uses code adjusted from the "Caching the Mean of a 
+# Vector" sample, provided at the Programming Assignment 2 in the
+# course R Programming at Coursera.
+
+## EXAMPLE
+
+# x <- matrix(1:4, 2,2)
+# m <- makeMatrix(x)
+# cacheInverse(m)
+#
+#       [,1] [,2]
+#  [1,]   -2  1.5
+#  [2,]    1 -0.5
+#
+# cacheInverse(m)
+#
+#   getting cached data
+#       [,1] [,2]
+#  [1,]   -2  1.5
+#  [2,]    1 -0.5
